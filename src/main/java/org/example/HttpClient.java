ebd8496cccfa803c250d5546db687f4f790eebc8
TEKami13 <tobias.krogh@hotmail.com>
2023-09-16 15:04:18 +0200

    Made the readLine and started on statusLine, statusCode and status

--- src/main/java/org/example/HttpClient.java
+++ src/main/java/org/example/HttpClient.java
@@ -2,8 +2,15 @@ package main.java.org.example;
 
 import java.io.IOException;
 import java.net.Socket;
+import java.util.HashMap;
+import java.util.Map;
 
 public class HttpClient {
+
+    public final int statusCode;
+    public final Map<String, String> headers = new HashMap<>();
+    public final String status;
+    public final String body;
 
 
     public HttpClient(String host, int port, String url) {
@@ -12,8 +19,30 @@ public class HttpClient {
             String request = String.format("GET %s HTTP/1.1\r\nHost: %s \r\n\r\n", url, host);
 
             socket.getOutputStream().write(request.getBytes());
+
+            String[] statusLine = readLine(socket).split(" ", 3);
+            statusCode = Integer.parseInt(statusLine[1]);
+            status = statusLine[2];
+
+
+
+
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private String readLine(Socket socket) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        int character;
+
+        while ( (character = socket.getInputStream().read() ) != '\r') {
+            sb.append(character);
+        }
+        character = socket.getInputStream().read();
+        if (character != '\n') {
+            throw new IllegalStateException("Invalid http header - \\r not followed by \\n");
+        }
+        return sb.toString();
     }
 }
---
+++ src/test/java/HttpClientTest.java
@@ -0,0 +1,17 @@
+package test.java;
+
+import main.java.org.example.HttpClient;
+import org.junit.jupiter.api.Test;
+
+public class HttpClientTest {
+
+    @Test
+    void shouldMakeHttpCallAndGet200Response() {
+        HttpClient client = new HttpClient("httpbin.org", 80, "/html");
+
+        assert client.statusCode == 200;
+        assert client.headers.get("Content-Type") != null;
+
+    }
+
+}
