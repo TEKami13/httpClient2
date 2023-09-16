4d8561e362b37fe8682c459d0385085101f94880
TEKami13 <tobias.krogh@hotmail.com>
2023-09-16 14:22:14 +0200

    created a socket and a request

--- src/main/java/org/example/HttpClient.java
+++ src/main/java/org/example/HttpClient.java
@@ -1,5 +1,19 @@
 package main.java.org.example;
 
+import java.io.IOException;
+import java.net.Socket;
+
 public class HttpClient {
 
+
+    public HttpClient(String host, int port, String url) {
+        try (Socket socket = new Socket(host, port)) {
+
+            String request = String.format("GET %s HTTP/1.1\r\nHost: %s \r\n\r\n", url, host);
+
+            socket.getOutputStream().write(request.getBytes());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
