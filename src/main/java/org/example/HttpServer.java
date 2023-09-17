8951dc5c66cb5c49a9ad6324178cd1ab39092712
TEKami13 <tobias.krogh@hotmail.com>
2023-09-17 18:20:04 +0200

    Added a modified version of the HttpClient's readLine to HttpServer

--- src/main/java/org/example/HttpServer.java
+++ src/main/java/org/example/HttpServer.java
@@ -1,4 +1,30 @@
 package main.java.org.example;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class HttpServer {
+
+    ServerSocket serverSocket;
+
+    public HttpServer(int port) throws IOException {
+        this.serverSocket = new ServerSocket(port);
+
+        Socket clientSocket = serverSocket.accept();
+        String requestLine = readLine(clientSocket);
+
+
+    }
+
+    private String readLine(Socket socket) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        int character;
+
+        while ((character = socket.getInputStream().read()) != '\r') {
+            sb.append((char)character); //char is important
+        }
+        character = socket.getInputStream().read();
+        return sb.toString();
+    }
 }
