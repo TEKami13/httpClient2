ab536583c5791d5dc996692c5c8062d59eacc891
TEKami13 <tobias.krogh@hotmail.com>
2023-09-17 20:13:59 +0200

    Not getting text in browser, but no error at least

--- src/main/java/org/example/HttpServer.java
+++ src/main/java/org/example/HttpServer.java
@@ -3,18 +3,38 @@ package main.java.org.example;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
 
 public class HttpServer {
 
-    ServerSocket serverSocket;
+    public final ServerSocket serverSocket;
+    public final String requestLine;
+    public final Map<String, String> headers;
 
     public HttpServer(int port) throws IOException {
         this.serverSocket = new ServerSocket(port);
+        headers = new HashMap<>();
 
         Socket clientSocket = serverSocket.accept();
-        String requestLine = readLine(clientSocket);
-        System.out.println(requestLine);
+        requestLine = readLine(clientSocket);
 
+        String headerLine;
+        while (!(headerLine = readLine(clientSocket)).isEmpty()) {
+            String[] headerParts = headerLine.split(" *: *", 2);
+            headers.put(headerParts[0], headerParts[1]);
+        }
+
+        String responseLine = "HTTP/1.1 200 OK\r\n";
+        String body = "<HTML><H1>OH HAI IAM A SERVER</H1></HTML>";
+        String header1 = "Content-Type:text/plain";
+        String header2 = "Content-Length:" + body.getBytes().length;
+        String spacer = "\r\n\r";
+
+        clientSocket.getOutputStream().write((responseLine + header1 + header2 + spacer + body).getBytes());
+
+        printThings();
 
     }
 
@@ -27,5 +47,10 @@ public class HttpServer {
         }
         character = socket.getInputStream().read();
         return sb.toString();
+    }
+
+    private void printThings() {
+        System.out.println(requestLine);
+        headers.entrySet().forEach(System.out::println);
     }
 }
