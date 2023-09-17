8d327ef8123c72d864261c67e7428c140790b038
TEKami13 <tobias.krogh@hotmail.com>
2023-09-17 20:31:23 +0200

    trying out a reload counter to show a different message

--- src/main/java/org/example/HttpServer.java
+++ src/main/java/org/example/HttpServer.java
@@ -12,6 +12,7 @@ public class HttpServer {
     public final ServerSocket serverSocket;
     public String requestLine;
     public final Map<String, String> headers;
+    public int counter = 0;
 
     public HttpServer(int port) throws IOException {
         this.serverSocket = new ServerSocket(port);
@@ -25,22 +26,38 @@ public class HttpServer {
             String[] headerParts = headerLine.split(" *: *", 2);
             headers.put(headerParts[0], headerParts[1]);
         }
-
+        if (counter <= 5) {
         String responseLine = "HTTP/1.1 200 OK\r\n";
         String body = "<HTML><H1>OH HAI IAM A SERVER</H1></HTML>";
         String header1 = "Content-Type:text/html\r\n";
         String header2 = "Content-Length:" + body.getBytes().length + "\r\n";
         String spacer = "\r\n";
 
-        clientSocket.getOutputStream().write((responseLine +
-                header1 +
-                header2 +
-                spacer +
-                body).getBytes());
+            clientSocket.getOutputStream().write((responseLine +
+                    header1 +
+                    header2 +
+                    spacer +
+                    body).getBytes());
+        } else {
+            String responseLine = "HTTP/1.1 200 OK\r\n";
+            String body = "<HTML><H1>OH HAI IAM A SERVER number 2</H1></HTML>";
+            String header1 = "Content-Type:text/html\r\n";
+            String header2 = "Content-Length:" + body.getBytes().length + "\r\n";
+            String spacer = "\r\n";
+
+            clientSocket.getOutputStream().write((responseLine +
+                    header1 +
+                    header2 +
+                    spacer +
+                    body).getBytes());
+        }
+
+
         clientSocket.getOutputStream().close();
         clientSocket.close();
 
         printThings();
+        counter++;
         }
     }
 
