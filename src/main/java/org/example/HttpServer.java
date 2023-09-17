e407ec7eb85a3eadfbbbaf795be317b2d8553e35
TEKami13 <tobias.krogh@hotmail.com>
2023-09-17 20:25:34 +0200

    Working server with while loop that keeps it from closing

--- src/main/java/org/example/HttpServer.java
+++ src/main/java/org/example/HttpServer.java
@@ -10,13 +10,13 @@ import java.util.Map;
 public class HttpServer {
 
     public final ServerSocket serverSocket;
-    public final String requestLine;
+    public String requestLine;
     public final Map<String, String> headers;
 
     public HttpServer(int port) throws IOException {
         this.serverSocket = new ServerSocket(port);
         headers = new HashMap<>();
-
+        while (true) {
         Socket clientSocket = serverSocket.accept();
         requestLine = readLine(clientSocket);
 
@@ -28,14 +28,20 @@ public class HttpServer {
 
         String responseLine = "HTTP/1.1 200 OK\r\n";
         String body = "<HTML><H1>OH HAI IAM A SERVER</H1></HTML>";
-        String header1 = "Content-Type:text/plain";
-        String header2 = "Content-Length:" + body.getBytes().length;
-        String spacer = "\r\n\r";
+        String header1 = "Content-Type:text/html\r\n";
+        String header2 = "Content-Length:" + body.getBytes().length + "\r\n";
+        String spacer = "\r\n";
 
-        clientSocket.getOutputStream().write((responseLine + header1 + header2 + spacer + body).getBytes());
+        clientSocket.getOutputStream().write((responseLine +
+                header1 +
+                header2 +
+                spacer +
+                body).getBytes());
+        clientSocket.getOutputStream().close();
+        clientSocket.close();
 
         printThings();
-
+        }
     }
 
     private String readLine(Socket socket) throws IOException {
