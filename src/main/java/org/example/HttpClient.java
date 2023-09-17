2e6c5d9ea2f3162f58b5f362d3668b5423b82078
TEKami13 <tobias.krogh@hotmail.com>
2023-09-17 16:12:05 +0200

    Made the readLine accept the input by turning character to char

--- src/main/java/org/example/HttpClient.java
+++ src/main/java/org/example/HttpClient.java
@@ -2,6 +2,7 @@ package main.java.org.example;
 
 import java.io.IOException;
 import java.net.Socket;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -10,17 +11,17 @@ public class HttpClient {
     public final int statusCode;
     public final Map<String, String> headers = new HashMap<>();
     public final String status;
-    public final String body;
-
+    public final String body = "";
 
     public HttpClient(String host, int port, String url) {
         try (Socket socket = new Socket(host, port)) {
 
-            String request = String.format("GET %s HTTP/1.1\r\nHost: %s \r\n\r\n", url, host);
+            String request = "GET " + url + " HTTP/1.1\r\n" +
+                    "Host: " + host + "\r\n\r\n";
 
             socket.getOutputStream().write(request.getBytes());
 
-            String[] statusLine = readLine(socket).split(" ", 3);
+            String[] statusLine = readLine(socket).split(" ");
             statusCode = Integer.parseInt(statusLine[1]);
             status = statusLine[2];
 
@@ -36,8 +37,8 @@ public class HttpClient {
         StringBuilder sb = new StringBuilder();
         int character;
 
-        while ( (character = socket.getInputStream().read() ) != '\r') {
-            sb.append(character);
+        while ((character = socket.getInputStream().read()) != '\r') {
+            sb.append((char)character); //char is important
         }
         character = socket.getInputStream().read();
         if (character != '\n') {
